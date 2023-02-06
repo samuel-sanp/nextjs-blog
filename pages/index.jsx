a60bd51ffc4c109b4aa6ec7dc69cf962898b783b
Samuel Constantino <samuelicapuidados@gmail.com>
2023-02-06 09:45:23 -0300

    fetch initial data

---
+++ pages/index.jsx
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../utils/post';
+
+export default function Home({ allPostData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Postagens</p>
+        <p>
+          <a href="http://localhost:3000/posts/first-post">Primeiro Post</a>
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br/>
+              {id}
+              <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostData = getSortedPostsData();
+  return {
+    props: {
+      allPostData,
+    }
+  }
+}
