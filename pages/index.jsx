3e768baab073e214be44b4776b48b089f760751f
Samuel Constantino <samuelicapuidados@gmail.com>
2023-02-07 09:51:34 -0300

    implements dynamic routes.

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../utils/post';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostData }) {
   return (
@@ -9,22 +11,16 @@ export default function Home({ allPostData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Postagens</p>
-        <p>
-          <a href="http://localhost:3000/posts/first-post">Primeiro Post</a>
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br/>
-              {id}
-              <br/>
-              {date}
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
